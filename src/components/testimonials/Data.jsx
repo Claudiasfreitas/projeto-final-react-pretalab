e91f98071bbf6fb5d2f0ee589b8b4cc7f75c109e
https://github.com/ClaudiadsFreitas <claudia.freita@icloud.com>
2022-12-17 00:02:49 -0300

    atualizando projeto final

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,6 +1,6 @@
-import Image1 from "../../assets/testimonial1.png";
-import Image2 from "../../assets/testimonial2.png";
-import Image3 from "../../assets/testimonial3.png";
+import Image1 from "../../assets/testimonial1.jpg";
+import Image2 from "../../assets/testimonial2.jpg";
+import Image3 from "../../assets/testimonial3.jpg";
 
 export const Data = [
   {
